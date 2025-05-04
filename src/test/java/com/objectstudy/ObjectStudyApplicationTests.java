a7f21256e991b7b1ca6713af567c048bc060df24
byunyourim <byourim@gmail.com>
2025-05-04 11:26:45 +0900

    init commit

---
+++ src/test/java/com/objectstudy/ObjectStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.objectstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
